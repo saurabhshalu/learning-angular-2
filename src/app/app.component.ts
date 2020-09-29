e721dc3bcdf65578d6a390431b76715668a4ea50
Brinda AN <brinda.an24@gmail.com>
2020-09-29 17:07:45 +0530

    added Update Function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   taskData:string="";
-  taskList=[];
+  taskList=[{title: 't1', done: false},{title: 't2', done: true},{title: 't3', done: false}];
+
+  isEditMode: boolean = false;
+  editIndex: number = -1;
 
   addTask(){
     let newTask={title:this.taskData , done:false};
@@ -15,7 +18,21 @@ export class AppComponent {
     this. taskData="";
   }
 
-  updateTask(sts,index){
+  editTask(index) {
+    let len=this.taskList.length;
+    this.taskData = this.taskList[len-index-1].title;
+    this.isEditMode = true;
+    this.editIndex=len-index-1;
+  }
+
+  updateTask() {
+    this.taskList[this.editIndex].title=this.taskData;
+    this.isEditMode = false;
+    this.editIndex=-1;
+    this.taskData='';
+  }
+
+  updateTaskState(sts,index){
     let len=this.taskList.length;
     this.taskList[len-index-1].done=sts;
   }
@@ -26,6 +43,6 @@ export class AppComponent {
 
   deleteTask(index){
     let len=this.taskList.length;
-    this.taskList.splice(len-index,1);
+    this.taskList.splice(len-index-1,1);
   }
 }
